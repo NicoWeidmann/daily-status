19f1aa9c9cfbaaeb730f3671c59ee7708bb61748
Nico Weidmann <nicoweidmann@web.de>
2018-03-26 01:06:58 +0200

    Implement most of weather module
    made a small change to the way the TIME_LIMIT is stored in the
    config. This was done to make working with it easier.
    
    Also implemented a first proof-of-concept output (a simple sentence
    for the current day).

--- config_example.py
+++ config_example.py
@@ -1,7 +1,9 @@
+from datetime import time
+
 # this is the time during a day after which information for the next day
 # will be printed instead of the current day
-# format as 'HH:MM:SS'
-TIME_LIMIT = '18:00:00'
+# format as 'HH, MM, SS'
+TIME_LIMIT = time(18, 00, 00)
 
 # this is the API key used for the darksky.net weather API
 DARKSKY_KEY = 'YOUR API KEY HERE'
--- weather.py
+++ weather.py
@@ -1,5 +1,18 @@
 import requests
-import config
+import sys
+import datetime
+try:
+    import config
+except ImportError as e:
+    # config file is missing
+    print('Error:', e)
+    print('Please create a config file'
+          ' (see "config_example.py" for instructions on how to do this) and restart the program.')
+    sys.exit(1)
+
+# set locale to german
+import locale
+locale.setlocale(locale.LC_ALL, 'de_DE')
 
 
 class Weather:
@@ -18,5 +31,45 @@ class Weather:
                                             config.DARKSKY_KEY, lat, long)
 
     def execute(self, printer):
+        # perform API request
         r = requests.get(self.api_url, params=self.params)
-        print(r.text)
+
+        if r.status_code != 200:
+            print("Error: Couldn't reach darksky API for weather information")
+            return
+
+        json = r.json()
+
+        # calculate UNIX timestamp of the data that should be extracted from the response
+        # TODO: make sure this works with all time zones
+        target_date = datetime.datetime.now()
+        target_date = target_date.replace(hour=0, minute=0, second=0, microsecond=0)
+        if datetime.datetime.time(datetime.datetime.now()) >= config.TIME_LIMIT:
+            # we print information for the next day
+            target_date += datetime.timedelta(days=1)
+
+        timestamp = target_date.timestamp()
+
+        forecast_data = None
+        for current_day in json['daily']['data']:
+            if current_day['time'] == timestamp:
+                # found correct day
+                forecast_data = current_day
+                break
+        else:
+            # couldn't find appropriate data for the desired day
+            print("Error: Couldn't acquire weather data for", target_date.isoformat)
+            return
+
+        forecast_text = ("Wettervorhersage für {:%A}: {summary} Die Temperatur beträgt zwischen {temperatureLow}°C "
+                         "und {temperatureHigh}°C. Die Niederschlagswahrscheinlichkeit beträgt {precipProbability}%.")
+
+        # calculate percentage
+        try:
+            forecast_data['precipProbability'] *= 100
+            forecast_text = forecast_text.format(target_date, **forecast_data)
+        except KeyError:
+            print("Error: acquired weather data is missing some required attributes")
+            forecast_text = "Es ist ein Fehler beim Laden des Wetters aufgetreten."
+
+        print(forecast_text)
