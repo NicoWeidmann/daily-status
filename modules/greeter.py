9b404fbd09d783e909f3b77e3a6fd4f9cdfee9d0
Nico Weidmann <nicoweidmann@web.de>
2018-03-26 21:19:02 +0200

    Print to provided printer in greeter module
    The greeter module now prints to the provided printer, if one
    is provided.
    
    Unfortunately, I cannot test this right now, as I don't have access
    to the thermal printer.

--- modules/greeter.py
+++ modules/greeter.py
@@ -6,5 +6,20 @@ class Greeter:
     def __init__(self, name):
         self.name = name
 
-    def execute(self, printer):
-        print('Hallo {0}, hier ist dein Status für {1:%A}, den {1:%x}'.format(self.name, calculate_target_date()))
+    def execute(self, printer=None):
+        if printer is not None:
+            # we have a printer, might as well use it
+            printer.write('Hallo ')
+            printer.boldOn()
+            printer.write(self.name)
+            printer.boldOff()
+            printer.write('. Hier ist dein Status für\n')
+            printer.justify('C')
+            printer.setSize('L')
+            printer.write('{:%A}\n'.format(calculate_target_date()))
+            printer.setSize('S')    # this is the default size
+            printer.write('{:%x}\n'.format(calculate_target_date()))
+            printer.justify('L')
+        else:
+            # we have no printer, print to stdout
+            print('Hallo {0}, hier ist dein Status für {1:%A}, den {1:%x}'.format(self.name, calculate_target_date()))
