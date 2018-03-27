6b05d43b39927d6241426a498f2d989b68fd8b4d
Nico Weidmann <nicoweidmann@web.de>
2018-03-28 00:42:27 +0200

    Make datetime returned by util tz-aware
    Added a config option for the local UTC offset that is used to
    calculate the local timezone. The datetime returned by
    util.calculate_target_date() is then made timezone aware to this
    timezone. This is useful for converting to UTC when performing
    requests to the Google Calendar API.

--- config_example.py
+++ config_example.py
@@ -1,9 +1,13 @@
-from datetime import time
+from datetime import time, timedelta
 
 # this is the time during a day after which information for the next day
 # will be printed instead of the current day
 # format as 'HH, MM, SS'
 TIME_LIMIT = time(18, 00, 00)
+
+# this is the UTC offset of your local timezone (used for UTC conversion in some modules)
+# TODO: find a more convenient solution to consider DST / different time zones
+UTC_OFFSET = timedelta(hours=2)
 
 # this is the API key used for the darksky.net weather API
 DARKSKY_KEY = 'YOUR API KEY HERE'
--- util.py
+++ util.py
@@ -5,8 +5,7 @@ import config
 # calculate the date the report should be generated for.
 # The date depends on the value set in config.TIME_LIMIT and the current system time.
 def calculate_target_date():
-    # TODO: make sure this works with all time zones
-    target_date = datetime.datetime.now()
+    target_date = datetime.datetime.now(tz=datetime.timezone(config.UTC_OFFSET))
     target_date = target_date.replace(hour=0, minute=0, second=0, microsecond=0)
     if datetime.datetime.time(datetime.datetime.now()) >= config.TIME_LIMIT:
         # we print information for the next day
