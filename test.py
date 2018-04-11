500d7d7407b44db78473e07101908ea348d7ad7d
Nico Weidmann <nicoweidmann@web.de>
2018-04-11 23:09:42 +0200

    Add calendar to the test script

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from modules import greeter
+from modules.calendar import googlecalendar as cal
 from lib.printer.Adafruit_Thermal import Adafruit_Thermal
 
 
 printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
-modules = [greeter.Greeter(name='Nico')]
+modules = [greeter.Greeter(name='Nico'), cal.Calendar()]
 
 {module.execute(printer) for module in modules}
