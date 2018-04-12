26ee3d42cf1ce8d90904a96642a4df621ef0bc34
Nico Weidmann <nicoweidmann@web.de>
2018-04-12 17:18:55 +0200

    Fix bug where status for the wrong day is printed
    Fixed a bug in util.calculate_target_date where the wrong day is
    returned during the UTC_OFFSET hours after midnight.

--- util.py
+++ util.py
@@ -5,9 +5,9 @@ import config
 # calculate the date the report should be generated for.
 # The date depends on the value set in config.TIME_LIMIT and the current system time.
 def calculate_target_date():
-    target_date = datetime.datetime.now(tz=datetime.timezone(config.UTC_OFFSET))
-    target_date = target_date.replace(hour=0, minute=0, second=0, microsecond=0)
-    if datetime.datetime.time(datetime.datetime.now()) >= config.TIME_LIMIT:
+    current_time = datetime.datetime.now(tz=datetime.timezone(config.UTC_OFFSET))
+    target_date = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
+    if current_time.time >= config.TIME_LIMIT:
         # we print information for the next day
         target_date += datetime.timedelta(days=1)
     return target_date
